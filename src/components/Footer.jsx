cc878dac2cf8bb0a97a0bb4e85067b49a9fa6914
Vedant Srivastava <vedsrivastava99@gmail.com>
2022-12-28 17:11:44 +0530

    changes for responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,9 @@ const Footer = () => {
   return (
     <footer>
         <div>
-            <h1>TechyStar</h1>
+            <h1>The Magic Touch</h1>
+            <p> By Vedant Srivastava</p>
+
             <p>@all right reserved</p>
         </div>
         <div>
