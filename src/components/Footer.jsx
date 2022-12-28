d537c709f6722f72f626fd67a653bf9a799f2c4a
Vedant Srivastava <vedsrivastava99@gmail.com>
2022-12-28 15:18:37 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>TechyStar</h1>
+            <p>@all right reserved</p>
+        </div>
+        <div>
+            <h5>Follow us</h5>
+            <div>
+                <a href="https://in.linkedin.com/" target={"blank"}>Linkdin</a>
+                <a href="https://www.instagram.com/" target={"blank"}>Instagram</a>
+                <a href="https://github.com/VedantSrivastava03" target={"blank"}>Github</a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
